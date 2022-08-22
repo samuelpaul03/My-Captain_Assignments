3fe134fd22c2fc813ad7c896bef4d0571fa11d3f
samuelpaul03 <110973181+samuelpaul03@users.noreply.github.com>
2022-08-22 10:23:51 +0530

    Add files via upload

--- Area of Circle.py
+++ Area of Circle.py
@@ -1,5 +1,4 @@
-Python program which accepts the radius of a circle from the user and computes area.
-
-from math import pi
-r = float(input ("Input the radius of the circle : "))
-print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+#AREA OF THE CIRCLE
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
